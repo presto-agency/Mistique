dab5192d4da40431c4664883a5a8a4529b39d9b6
Kamarhinanton <Kamarhin_anton@ukr.net>
2023-05-03 16:59:09 +0300

    feat: add fonts, etc to project

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,12 +1,20 @@
-import { Html, Head, Main, NextScript } from 'next/document'
+import {Html, Head, Main, NextScript} from 'next/document'
 
 export default function Document() {
   return (
     <Html lang="en">
-      <Head />
+      <Head>
+        <link rel="icon" href="/favicon.svg" type="image/svg+xml" />
+        <link rel="preload" href="fonts/EBGaramond-Regular.woff" as="font" type="font/woff" crossOrigin="anonymous"/>
+        <link rel="preload" href="fonts/EBGaramond-Regular.woff2" as="font" type="font/woff2" crossOrigin="anonymous"/>
+        <link rel="preload" href="fonts/EBGaramond-Medium.woff" as="font" type="font/woff" crossOrigin="anonymous"/>
+        <link rel="preload" href="fonts/EBGaramond-Medium.woff2" as="font" type="font/woff2" crossOrigin="anonymous"/>
+        <link rel="preload" href="fonts/EBGaramond-SemiBold.woff" as="font" type="font/woff" crossOrigin="anonymous"/>
+        <link rel="preload" href="fonts/EBGaramond-SemiBold.woff2" as="font" type="font/woff2" crossOrigin="anonymous"/>
+      </Head>
       <body>
-        <Main />
-        <NextScript />
+      <Main/>
+      <NextScript/>
       </body>
     </Html>
   )
