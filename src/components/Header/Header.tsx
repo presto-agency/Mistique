6cb48bfa5107ec1cb6a8b6700055112c48778f3b
Kamarhinanton <Kamarhin_anton@ukr.net>
2023-05-09 18:10:00 +0300

    feat: add burger animation

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,14 +1,15 @@
-import React from 'react';
+import React, {useState} from 'react';
 import * as styles from "./header.module.scss";
 import Link from "next/link";
 import Button from "@/components/UI/Button/Button";
 import {useClassMobile} from "@/hooks/useClassMobile";
 
-
 const Header = () => {
   const isMobile = useClassMobile(false);
+  const [isActive, setIsActive] = useState<boolean>(false);
+
   const headerActive = () => {
-    console.log('hi')
+    setIsActive(prevState => !prevState);
   }
 
   return (
@@ -17,26 +18,26 @@ const Header = () => {
         ? "/images/curtain-top-mob.png"
         : "/images/curtain-top.png"
       } className="curtain curtain-top"></img>
-      <header className={styles.header}>
+      <header className={`${styles.header} ${isActive ? styles.active : ''}`}>
         <div className="container">
           {isMobile
             ? <nav className={styles.header__content}>
-                <Link className={styles.header__content_logo} href='/'>
-                  <img src="/images/eye.svg" alt="logo"/>
-                </Link>
-                <div className={styles.header__content_burger}>
-                  <span></span>
-                  <span></span>
-                  <span></span>
-                </div>
-              </nav>
+              <Link className={styles.header__content_logo} href='/'>
+                <img src="/images/eye.svg" alt="logo"/>
+              </Link>
+              <div onClick={headerActive} className={styles.header__content_burger}>
+                <span></span>
+                <span></span>
+                <span></span>
+              </div>
+            </nav>
             : <nav className={styles.header__content}>
-                <Link className={styles.header__content_text} href='/'>Join Mystique</Link>
-                <div className={styles.header__content_links}>
-                  <Button className={'button _small'} title={'Go Tarot'}/>
-                  <Button className={'button _small _icon button-dark'} title={'Menu'}/>
-                </div>
-              </nav>
+              <Link className={styles.header__content_text} href='/'>Join Mystique</Link>
+              <div className={styles.header__content_links}>
+                <Button className={'button _small'} title={'Go Tarot'}/>
+                <Button className={'button _small _icon button-dark'} title={'Menu'}/>
+              </div>
+            </nav>
           }
         </div>
       </header>
