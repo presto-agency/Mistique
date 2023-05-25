6342cf2d432329b61cf4b439e66602e06cf75b79
Kamarhinanton <Kamarhin_anton@ukr.net>
2023-05-25 22:16:49 +0300

    fix: growingStars refactor

--- src/components/GlowingStars/GlowingStars.tsx
+++ src/components/GlowingStars/GlowingStars.tsx
@@ -2,13 +2,14 @@ import React from "react";
 import styles from "./glowingStars.module.scss";
 
 const GlowingStars = () => {
-  return (
-    <div className={styles.stars}>
-      {Array.from({ length: 5 }).map((_, index) => (
-        <div key={index} className={styles.stars__star}></div>
-      ))}
-    </div>
-  );
+
+  const stars = Array.from({length: 5}, (_, index) =>
+    <div
+      key={index}
+      className={styles.stars__star}>
+    </div>);
+
+  return <>{stars}</>;
 };
 
 export default GlowingStars;
